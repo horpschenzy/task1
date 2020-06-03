e8c9f77d1fc1004e6fda88e546ca7ad061b16826
Horpschenzy <opeoluwa.lanre@gmail.com>
2020-06-03 04:26:14 +0100

    added flush to index.php and make a test file in scripts  folder and worked on my

--- scripts/horp.php
+++ scripts/horp.php
@@ -2,4 +2,4 @@
 $name = 'Horpschenzy';
 $id = 1234;
 $lang = "PHP";
-print('Hello World, this is '.$name.' with HNGi7 ID $id using '.$lang.' for stage 2 task');
+print('Hello World, this is '.$name.' with HNGi7 ID '.$id.' using '.$lang.' for stage 2 task');
